c59c19f4d8c42dcb4c2ac25f3692c1c7318adeee
Swaraj Phalke <swarajphalke555@gmail.com>
2025-04-27 23:29:19 +0530

    Skills Section Updated

--- components/sidebar/Skills.tsx
+++ components/sidebar/Skills.tsx
@@ -15,8 +15,9 @@ export default function Skills() {
         "Bootstrap",
         // "Sass",
         "React",
+        "TypeScript",
         "Material UI",
-        "Redux",
+        // "Redux",
       ],
     },
     {
